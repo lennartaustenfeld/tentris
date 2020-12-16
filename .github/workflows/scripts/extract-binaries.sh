17f30b116a8103684e26a2d1773b9fc0574e2f9d
Lennart Austenfeld <lennarta@mail.upb.de>
2020-12-16 17:27:12 +0100

    Create release

--- .github/workflows/scripts/extract-binaries.sh
+++ .github/workflows/scripts/extract-binaries.sh
@@ -6,5 +6,7 @@ docker cp ${container_id}:/tentris_terminal ./tentris_terminal
 docker cp ${container_id}:/rdf2ids ./rdf2ids
 docker cp ${container_id}:/ids2hypertrie ./ids2hypertrie
 tag=$(echo $GITHUB_REF | rev | cut -d'/' -f1 | rev)
-zip benchmarktools_${tag}_clang9_libstdcxx9.zip rdf2ids ids2hypertrie
-zip tentris_${tag}_clang9_libstdcxx9.zip tentris_server tentris_terminal
+zip benchmarktools_clang9_libstdcxx9.zip rdf2ids ids2hypertrie
+zip tentris_clang9_libstdcxx9.zip tentris_server tentris_terminal
+#zip benchmarktools_${tag}_clang9_libstdcxx9.zip rdf2ids ids2hypertrie
+#zip tentris_${tag}_clang9_libstdcxx9.zip tentris_server tentris_terminal
